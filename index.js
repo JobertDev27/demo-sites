f4999aed06f7db37cc873507b5de46669cef6462
JobertDev27 <jobertdev27@gmail.com>
2025-06-16 23:15:24 +0800

    style: fix mobile layout

--- index.js
+++ index.js
@@ -10,7 +10,24 @@ const observer = new IntersectionObserver(
   { threshold: 0.3 }
 );
 
+const lesserObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("reveal");
+        observer.unobserve(entry);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
 const hiddenElems = document.querySelectorAll(".hidden");
 hiddenElems.forEach((elem) => {
   observer.observe(elem);
+});
+
+const lessHiddenElems = document.querySelectorAll(".less-hidden");
+lessHiddenElems.forEach((elem) => {
+  lesserObserver.observe(elem);
 });
